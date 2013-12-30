1f423ae3d1f9f89b7830708e2837e63e160eecb2
Frode Aabak <frode_aabak@hotmail.com>
2013-12-30 19:20:08 +0100

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/hltexx/full_hltexx.mk)
+$(call inherit-product, device/samsung/ks01lte/ks01ltexx.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -10,8 +10,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltexx TARGET_DEVICE=hltexx BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=ks01lte TARGET_DEVICE=ks01lte BUILD_FINGERPRINT="samsung/ks01lte/ks01lte:4.3/JSS15J/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="ks01ltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
 
-PRODUCT_DEVICE := hltexx
-PRODUCT_NAME := cm_hltexx
+PRODUCT_DEVICE := ks01lte
+PRODUCT_NAME := cm_ks01lte
 
